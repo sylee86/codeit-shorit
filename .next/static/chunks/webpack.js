54acd5ca0d5e62c988d0c538c1c5ecac4460edca
sylee <linggo01@naver.com>
2025-11-13 13:04:56 +0900

    ✨ vercel - mongoDB연결

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "88c31ae6d186d90d"; }
+/******/ 		__webpack_require__.h = function() { return "7aff492311ce85f0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/api/short-links/[id].js
+++ pages/api/short-links/[id].js
@@ -1,22 +1,25 @@
-import dbConnect from '@/db/dbConnect';
-import ShortLink from '@/db/models/ShortLink';
+import dbConnect from "@/db/dbConnect";
+import ShortLink from "@/db/models/ShortLink";
 
 export default async function handler(req, res) {
   await dbConnect();
   const { id } = req.query;
 
   switch (req.method) {
-    case 'GET':
+    case "GET":
       const shortLink = await ShortLink.findById(id);
       res.send(shortLink);
       break;
 
-    case 'PATCH':
-      const updatedShortLink = await ShortLink.findByIdAndUpdate(id, req.body, { new: true });
+    case "PATCH":
+      const updatedShortLink = await ShortLink.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+
       res.send(updatedShortLink);
       break;
 
-    case 'DELETE':
+    case "DELETE":
       await ShortLink.findByIdAndDelete(id);
       res.status(204).send();
       break;
